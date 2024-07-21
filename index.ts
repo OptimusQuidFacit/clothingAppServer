69871ee1d778ea65bf11270afe19ece475383a37
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-07-21 18:03:00 +0100

    code revamp

--- index.ts
+++ index.ts
@@ -1,118 +1,130 @@
-import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { buildSchema } from 'graphql';
+import cart from './models/cart';
 
-// const { graphqlHTTP } = require("express-graphql");
-const mongoose = require("mongoose");
-const cartModel = require('./models/cart');
-const express = require('express');
-const { buildSchema } = require('graphql');
-const app= express();
-const dotenv = require ("dotenv");
 dotenv.config();
 
+const app = express();
+
 mongoose.connect(process.env.MONGO_URL as string)
-.then(()=>{
+  .then(() => {
     console.log('Successful connection');
-})
-.catch((err: any)=>console.log(err));
+  })
+  .catch((err: any) => console.log(err));
 
-type cartType={
-    id: number,
-    name: string
-    unitPrice:number,
-    quantity: number,
-    total: number,
-    size?: string,
-    color?: string,
-    category?: string[],
-  }
+type CartType = {
+  productId: number;
+  userId: string;
+  name: string;
+  unitPrice: number;
+  quantity: number;
+  total: number;
+  size?: string;
+  color?: string;
+  category?: string[];
+  img?: number;
+};
+
 const products = [
-    {
-        name: 'Cotton Classic',
-        price: 15000,
-        category: ['Women', 'white'],
-        colors:['black', 'white', 'red'],
-        sizes: ['S','M', 'L', ],
-    },
-    {
-        name: 'Classic Men\'s Suit',
-        price: 25000,
-        category: ['Men', 'black'],
-        colors:['black', 'brown', 'grey'],
-        sizes: ['S','M', 'L', ],
-    },
-]
+  {
+    name: 'Cotton Classic',
+    price: 15000,
+    category: ['Women', 'white'],
+    colors: ['black', 'white', 'red'],
+    sizes: ['S', 'M', 'L'],
+  },
+  {
+    name: 'Classic Men\'s Suit',
+    price: 25000,
+    category: ['Men', 'black'],
+    colors: ['black', 'brown', 'grey'],
+    sizes: ['S', 'M', 'L'],
+  },
+];
+
 const schema = buildSchema(`
-    type product {
-        name: String!,
-        price: Float!,
-        category: [String!]!,
-        colors: [String!]!,
-        sizes: [String!]!
-    },
-    type cartItem {
-        productId: Float!,
-        userId: String!,
-        name: String!,
-        unitPrice:Float!,
-        quantity: Int!,
-        total: Float!,
-        size: String!,
-        color: String!,
-        category: [String!]!,
-    },
-    input cartItemInput {
-        productId: Float!,
-        userId: String!,
-        name: String!,
-        unitPrice:Float!,
-        quantity: Int!,
-        total: Float!,
-        size: String!,
-        color: String!,
-        category: [String!]!,
+  type Product {
+    name: String!
+    price: Float!
+    category: [String!]!
+    colors: [String!]!
+    sizes: [String!]!
+  }
+
+  type CartItem {
+    productId: Float!
+    userId: String!
+    name: String!
+    unitPrice: Float!
+    quantity: Int!
+    total: Float!
+    size: String!
+    color: String!
+    category: [String!]!
+  }
+
+  input CartItemInput {
+    productId: Float!
+    userId: String!
+    name: String!
+    unitPrice: Float!
+    quantity: Int!
+    total: Float!
+    size: String!
+    color: String!
+    category: [String!]!
+    img: Float!
+  }
+
+  type Query {
+    cart(userId: String): [CartItem]
+    products: [Product]
+  }
+
+  type Mutation {
+    addToCart(item: CartItemInput): CartItem
+    deleteFromCart(id: String): CartItem
+  }
+`);
+
+const root = {
+  products: () => products,
+  cart: async ({ userId }: { userId: string }) => {
+    return await cart.find({ userId }).exec();
+  },
+  addToCart: async ({ item }: { item: CartType }) => {
+    try {
+      const newCart = new cart(item);
+      const newCartItem = await newCart.save();
+      return newCartItem;
+    } catch (err) {
+      console.log(err);
+      return null;
     }
-    
-    type Query {
-        cart(userId: String): [cartItem]
-        product: [product]
-    },
-
-    type Mutation {
-        addToCart(item: cartItemInput): cartItem
-        deleteFromCart(id: String): cartItem
-    }    
-    `)
-
-
-    const root = {
-        product: ()=> products,
-        cart: async ({userId}: {userId: string}) => {return (await cartModel.find({userId: userId}).toArray())},
-        addCart: async ({cartItem}: {cartItem: cartType})=>{
-            try{
-                // await cartModel.insertOne(cartItem);
-                let cart = new cartModel(cartItem)
-                let newCartItem= await cart.save();
-                return newCartItem;
-            }
-            catch(err) {console.log(err)}
-        },
-        deleteFromCart: async ({id}:{id: string})=>{
-            try{
-                await cartModel.deleteOne({_id:id});
-            }
-            catch(err){
-                console.log(err);
-            }
-        }
+  },
+  deleteFromCart: async ({ id }: { id: string }) => {
+    try {
+      const deletedItem = await cart.findByIdAndDelete(id).exec();
+      return deletedItem;
+    } catch (err) {
+      console.log(err);
+      return null;
     }
-    app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-    }));
+  },
+};
 
-app.get('/', (req: Request, res: any)=>res.send('Server is running'))
-const port= process.env.PORT || 3000
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.get('/', (req: Request, res: Response) => res.send('Server is running'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Running a GraphQL API server at https://clothing-app-server.vercel.app');
+  console.log('Running a GraphQL API server at http://localhost:' + port);
 });
--- models/cart.ts
+++ models/cart.ts
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 const cartSchema= new mongoose.Schema(
     {
         productId: {type: Number, required:true, unique:true},
-        userId: {type: String, required:true, unique:true},
-        name: {type: String, required:true, unique:true},
-        unitPrice:{type: Number, required:true, unique:true},
-        quantity: {type: Number, required:true, unique:true},
-        total: {type: Number, required:true, unique:true},
-        size: {type: String, required:true, unique:true},
-        color: {type: String, required:true, unique:true},
-        category: {type: Array, required:true, unique:true},
+        userId: {type: String, required:true},
+        name: {type: String, required:true,},
+        unitPrice:{type: Number, required:true},
+        quantity: {type: Number, required:true,},
+        total: {type: Number, required:true,},
+        size: {type: String, required:true,},
+        color: {type: String, required:true,},
+        category: {type: Array, required:true,},
+        img: {type: Number},
+        
     }
 )
 
-module.exports = mongoose.model("cart", cartSchema);
+export default mongoose.model("cart", cartSchema);
