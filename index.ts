ec487adae3daa5b5daf46461da7f341e4bd6b67a
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-07-22 06:58:35 +0100

    reseetig mongoose.connect

--- index.ts
+++ index.ts
@@ -9,11 +9,17 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL as string)
-  .then(() => {
-    console.log('Successful connection');
-  })
-  .catch((err: any) => console.log(err));
+const connectToDatabase = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URL as string);
+      console.log('Successfully connected to MongoDB Atlas');
+    } catch (error) {
+      console.error('Error connecting to MongoDB Atlas:', error);
+      process.exit(1); // Exit process with failure
+    }
+  };
+
+  connectToDatabase();
 
 type CartType = {
   productId: number;
