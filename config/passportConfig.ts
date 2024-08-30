9ab443ca534b74e701ce18c672324ad5a666879c
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-08-30 11:49:30 +0100

    code verifier test 2

--- config/passportConfig.ts
+++ config/passportConfig.ts
@@ -1,122 +1,117 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const LocalStrategy= require('passport-local').Strategy
-const JwtStrategy= require('passport-jwt').Strategy
-const ExtractJwt= require('passport-jwt').ExtractJwt
+const LocalStrategy= require('passport-local').Strategy;
+const JwtStrategy= require('passport-jwt').Strategy;
+const ExtractJwt= require('passport-jwt').ExtractJwt;
 const usersModel = require('../models/user');
-const crypto = require('crypto')
+const crypto = require('crypto');
 import dotenv from "dotenv";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 dotenv.config();
 
+// Generate a code verifier and challenge
 const codeVerifier = crypto.randomBytes(32).toString('hex');
-
 const codeChallenge = crypto
   .createHash('sha256')
   .update(codeVerifier)
   .digest('base64url');
 
-const jwtOptions={
+// JWT options
+const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SEC
-}
+};
 
-module.exports= (passport:any)=>{
+module.exports = (passport:any) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/callback",
-        codeChallenge: codeChallenge, // Pass code challenge here
-        codeChallengeMethod: 'S256', // Required if using PKCE
+        passReqToCallback: true, // Enable passing request object to callback
+        codeChallengeMethod: 'S256' // Required if using PKCE
       },
-      async (accessToken:any, refreshToken:any, profile:any, cb:any) => {
-
-        // console.log(profile.id);
-        const newUser = new usersModel({
-            googleId: profile.id,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            displayName: profile.displayName,
-            image: profile.photos[0].value,
-        });
-        try{
-            let user = await usersModel.findOne({googleId: profile.id})
-
-            if(user){
-                cb(null, user)
+      async (req:any, accessToken:any, refreshToken:any, profile:any, cb:any) => {
+        try {
+            // Retrieve code_verifier from session
+            const codeVerifier = req.session.codeVerifier;
+            if (!codeVerifier) {
+                throw new Error("Missing code_verifier.");
             }
-            else{
+
+            // You can validate the codeVerifier here if needed
+            // e.g., check if it matches the original codeChallenge
+            // Generate a new user object
+            const newUser = new usersModel({
+                googleId: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                displayName: profile.displayName,
+                image: profile.photos[0].value,
+            });
+
+            // Check if user exists or save the new user
+            let user = await usersModel.findOne({ googleId: profile.id });
+            if (user) {
+                cb(null, user);
+            } else {
                 await newUser.save();
-                cb(null,user);
+                cb(null, user);
             }
-
-        }
-        catch(err){
+        } catch (err) {
             console.error(err);
+            cb(err, null);
         }
-
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        //   return cb(err, user);
-        // });
-    
       }
-    ))
+    ));
+
     passport.use(new LocalStrategy({
-        usernameField:'email'
+        usernameField: 'email'
     },
-        async (email:string, password:string, done:any)=> {
-           
-            try {
-
-                const user = await usersModel.findOne({ email: email }).lean()
-                if (!user) {
-               
-                    return done(null, false, {error:"User does not exist"}); 
-                }
-
-                let passwordIsCorrect= await bcrypt.compare(password, user.password)
-
-                if(!passwordIsCorrect){
-                    return done(null, false, {error:`Password is incorrect ${passwordIsCorrect}`});
-                }
-                return done(null, user);
+    async (email: string, password: string, done: any) => {
+        try {
+            const user = await usersModel.findOne({ email: email }).lean();
+            if (!user) {
+                return done(null, false, { error: "User does not exist" });
             }
-            catch(err){
-                return done(err);
+
+            const passwordIsCorrect = await bcrypt.compare(password, user.password);
+            if (!passwordIsCorrect) {
+                return done(null, false, { error: "Password is incorrect" });
             }
+
+            return done(null, user);
+        } catch (err) {
+            return done(err);
         }
-    ))
+    }));
 
-
-        passport.serializeUser((user:any, cb:any) => {
-         process.nextTick(()=> {
-          return cb(null, {
-            id: user.id,
-            username: user.username,
-            picture: user.picture
-          });
+    passport.serializeUser((user: any, cb: any) => {
+        process.nextTick(() => {
+            return cb(null, {
+                id: user.id,
+                username: user.username,
+                picture: user.picture
+            });
         });
-      });
-      
-      passport.deserializeUser((user:any, cb:any)=> {
-        process.nextTick(()=> {
-          return cb(null, user);
-        });
-      });
+    });
 
-      passport.use(
-        new JwtStrategy(jwtOptions, async (jwt_payload: any, done: any)=>{
-            try{
-                let user = usersModel.findOne({_id: jwt_payload.id})
-                if(user){
-                    return done(null, user)
+    passport.deserializeUser((user: any, cb: any) => {
+        process.nextTick(() => {
+            return cb(null, user);
+        });
+    });
+
+    passport.use(
+        new JwtStrategy(jwtOptions, async (jwt_payload: any, done: any) => {
+            try {
+                let user = await usersModel.findOne({ _id: jwt_payload.id });
+                if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
                 }
-                else{
-                    return done(null, false)
-                }
-            }
-            catch(err){
-                return done(err, false)
+            } catch (err) {
+                return done(err, false);
             }
         })
-      )
-}
+    );
+};
