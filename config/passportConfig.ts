6db7f7e2c207801596af117b4d1af5e141abde27
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-09-07 15:26:10 +0100

    passport fix 2

--- config/passportConfig.ts
+++ config/passportConfig.ts
@@ -21,7 +21,9 @@ const jwtOptions = {
     secretOrKey: process.env.JWT_SEC
 };
 
+
 module.exports = (passport:any) => {
+    //Google Strategy
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
